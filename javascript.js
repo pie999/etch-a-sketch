36abe627dc3aa4d0bbfde2a99bb851959a5bb474
pie999 <pietro.vergara@libero.it>
2022-05-29 18:24:13 +0200

    Implement button to change number of pixels per side

--- javascript.js
+++ javascript.js
@@ -1,15 +1,24 @@
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++){
-    const rowcont = document.createElement("div")
-    rowcont.classList.add("rowcont");
-    container.appendChild(rowcont)
-    for (let j = 0; j < 16; j++){
-        const pixel = document.createElement("div");
-        pixel.classList.add("pixel")
-        rowcont.appendChild(pixel);
-        pixel.addEventListener("mouseover", () => {
-            pixel.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
-        })
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    while(container.firstChild) 
+        container.removeChild(container.firstChild); //clean preavious canvas
+    pixelsPerSide = prompt("How many pixels on each side?");
+    while (pixelsPerSide < 1 || pixelsPerSide > 100){
+        alert("Number must be between 1 and 100");
+        pixelsPerSide = prompt("How many pixels on each side?");
     }    
-}
-
+    for (let i = 0; i < pixelsPerSide; i++){
+        const rowcont = document.createElement("div")
+        rowcont.classList.add("rowcont");
+        container.appendChild(rowcont)
+        for (let j = 0; j < pixelsPerSide; j++){
+            const pixel = document.createElement("div");
+            pixel.classList.add("pixel")
+            rowcont.appendChild(pixel);
+            pixel.addEventListener("mouseover", () => {
+                pixel.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+            });
+        }    
+    }
+});
